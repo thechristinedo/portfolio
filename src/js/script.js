4c8ad2a0064bf5bf892d265a0a7c8e09e0d31fe5
thechristinedo <thechristinedo@gmail.com>
2025-01-10 05:20:01 -0600

    added projects section and mobile navbar

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,12 @@
 const body = document.body;
-const themeBtn = document.querySelector(".navbar__list--button");
+const themeBtn = document.querySelector(".navbar__theme");
 const themeIcon = document.querySelector(".theme--icon");
+
+const logo = document.querySelector(".logo");
+const navMenu = document.querySelector(".navmenu");
+const openNavBtn = document.querySelector(".navbar__open");
+const navMenuLink = document.querySelectorAll(".navmenu__link");
+
 const trailLeft = document.querySelector(".trail__color--left");
 const trailRight = document.querySelector(".trail__color--right");
 
@@ -27,4 +33,31 @@ themeBtn.addEventListener("click", function () {
   body.classList.toggle("light--mode");
   toggleTrailColor();
   toggleIcon();
+});
+
+openNavBtn.addEventListener("click", function () {
+  navMenu.classList.toggle("hide");
+  body.classList.toggle("stop__scroll");
+  logo.classList.toggle("navmenu__link");
+});
+
+logo.addEventListener("click", function () {
+  if (!navMenu.classList.contains("hide")) {
+    navMenu.classList.toggle("hide");
+    body.classList.toggle("stop__scroll");
+  }
+});
+
+navMenuLink.forEach((link) => {
+  link.addEventListener("click", function () {
+    navMenu.classList.toggle("hide");
+    body.classList.toggle("stop__scroll");
+  });
+});
+
+window.addEventListener("resize", function () {
+  body.classList.remove("stop__scroll");
+  if (window.innerWidth < 50 * 16 && !navMenu.classList.contains("hide")) {
+    navMenu.classList.toggle("hide");
+  }
 });
